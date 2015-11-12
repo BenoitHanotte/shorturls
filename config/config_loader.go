a37b602b8d467c09e5f54f858d27ab74088deda2
Benoit <benoit.jc.hanotte@campus.tu-berlin.de>
2015-11-12 09:15:47 +0100

    improved config override with env variables

--- config/config_loader.go
+++ config/config_loader.go
@@ -4,11 +4,14 @@ import (
 	"errors"
 	log "github.com/BenoitHanotte/shorturls/Godeps/_workspace/src/github.com/Sirupsen/logrus"
 	"github.com/BenoitHanotte/shorturls/Godeps/_workspace/src/github.com/spf13/viper"
+	"os"
 )
 
 type Config struct {
-	RedisAddr string // the IP address of the redis node
-	RedisPort int    // the port of the redis node
+	Host		string	// the host to use (eg: toto.com), default: HOST env variable
+	Port 		int		// the port of the server
+	RedisAddr 	string 	// the IP address of the redis node
+	RedisPort 	int    	// the port of the redis node
 }
 
 // Load a YAML config file and put values in a Config object
@@ -27,16 +30,27 @@ func LoadConfigYAML(filename string) (*Config, error) {
 		return nil, errors.New("config file not found")
 	}
 
-	// set defaults
-	viper.SetDefault("RedisPort", 1234)
+	// overrides with environment variables (for debug, or to use docker links)
+	if os.Getenv("HOST")!="" {
+		viper.Set("host", os.Getenv("HOST"))
+	}
+	if os.Getenv("PORT")!="" {
+		viper.Set("port", os.Getenv("PORT"))
+	}
+	if os.Getenv("REDIS_PORT_6379_TCP_PORT")!="" {
+		viper.Set("redisPort", os.Getenv("REDIS_PORT_6379_TCP_PORT"))
+	}
+	if os.Getenv("REDIS_PORT_6379_TCP")!="" {
+		viper.Set("redisHost", os.Getenv("REDIS_PORT_6379_TCP"))
+	}
 
-	c := Config{
-		RedisAddr: viper.GetString("essai"),
-		RedisPort: viper.GetInt("RedisPort")}
+	config := Config{
+		Host:		viper.GetString("host"),
+		Port:		viper.GetInt("port"),
+		RedisAddr: 	viper.GetString("redisHost"),
+		RedisPort: 	viper.GetInt("redisPort")}
 
-	log.WithFields(log.Fields{
-		"RedisAddr": c.RedisAddr,
-		"RedisPort": c.RedisPort}).Debug("configuration loaded from YAML")
+	log.WithField("config", config).Debug("configuration loaded from YAML")
 
-	return &c, nil
+	return &config, nil
 }

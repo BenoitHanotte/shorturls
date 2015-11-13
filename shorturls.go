d0bb0d27666134c1d065bd3205e9d3a3965ffde4
Benoit <benoit.jc.hanotte@campus.tu-berlin.de>
2015-11-13 09:39:15 +0100

    moved log logic to function

--- shorturls.go
+++ shorturls.go
@@ -9,27 +9,15 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"fmt"
 )
 
-func init() {
-	// configure the logger
-	// The level can be changed with the LOG_LEVEL environment variable, default=info
-	level := os.Getenv("LOG_LEVEL")
-	switch level {
-	case "debug":
-		log.SetLevel(log.DebugLevel)
-	case "warn":
-		log.SetLevel(log.WarnLevel)
-	case "error":
-		log.SetLevel(log.ErrorLevel)
-	default:
-		log.SetLevel(log.InfoLevel)
-	}
-	// set the output
-	log.SetOutput(os.Stdout)
-}
-
 func main() {
+
+	// setUp the logger
+	toDefer := setUpLog()
+	// the returned function is to defer, used to close log file on exit
+	defer toDefer();
 
 	log.Info("starting")
 
@@ -67,4 +55,42 @@ func main() {
 		log.WithError(err).Fatal("could not start the router, exiting")
 		return
 	}
+}
+
+// set up the logrus logger
+// returns a function to defer, used to defer closing the file in which the logs are written
+func setUpLog() func() {
+	// configure the logger
+	// The level can be changed with the LOG_LEVEL environment variable, default=info
+	level := os.Getenv("LOG_LEVEL")
+	switch level {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.SetLevel(log.InfoLevel)
+	}
+
+	// set the logger output
+	logFile := os.Getenv("LOG_FILE")
+	switch logFile {
+	case "":
+		// also when not set (actual default)
+		log.SetOutput(os.Stderr)
+	default:
+		// consider any other value as a filepath
+		f, err := os.Create(logFile)
+		if err!=nil {
+			fmt.Errorf("Unable to open log file: "+err.Error())
+		}
+		log.SetOutput(f)
+		return func() {
+			defer f.Close()
+		}
+	}
+
+	return func() {}
 }
